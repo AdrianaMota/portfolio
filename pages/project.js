1c1ee6679ba0e9929b6d11922c934af9a7fb114b
AdrianaMota <lishamota@hotmail.com>
2022-06-25 23:41:25 -0400

    add package

--- pages/project.js
+++ pages/project.js
@@ -1,9 +1,10 @@
 import { Stack, Image } from "@chakra-ui/react";
+import { Carousel } from "../components/Slider/Carousel";
 
 export default function project() {
 	return (
 		<Stack spacing={0} pb="4">
-			<Image src="Logo.png" width={"60rem"} />
+			<Image src={handleClick} width={"60rem"} />
 		</Stack>
 	);
 }
