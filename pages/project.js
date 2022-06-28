63e8a58e19581f4faff5ecab2161c2036eb4d173
AdrianaMota <lishamota@hotmail.com>
2022-06-27 23:35:13 -0400

    QA Updates

--- pages/project.js
+++ pages/project.js
@@ -2,9 +2,12 @@ import { Stack, Image } from "@chakra-ui/react";
 import { Carousel } from "../components/Slider/Carousel";
 
 export default function project() {
+	 
+
+
 	return (
 		<Stack spacing={0} pb="4">
-			<Image src={handleClick} width={"60rem"} />
+			<Image src={Carousel} width={"60rem"} />
 		</Stack>
 	);
 }
