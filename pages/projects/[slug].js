9d4f2801e31b441cec1772b35509e3465379dfb6
AdrianaMota <lishamota@hotmail.com>
2022-06-30 23:54:49 -0400

    create callback function

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -4,7 +4,11 @@ import { projects } from "../../components/Slider/utils/projects";
 const ProjectBySlug = () => {
 	const { query } = useRouter();
 
-	// const currentProject = projects.find(callback query);
+	function isCurrent(img) {
+		return img.slug === query;
+	}
+
+	const currentProject = projects.find(isCurrent);
 
 	return <div>{JSON.stringify(query)}</div>;
 };
